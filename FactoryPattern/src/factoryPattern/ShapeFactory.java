9d18a5ad96fc84b340bff84ed77c8f30342c733d
amir.sojoodi <amir.sojoodi@gmail.com>
2018-04-18 02:27:12 +0430

    First design pattern: FactoryPattern

---
+++ FactoryPattern/src/factoryPattern/ShapeFactory.java
@@ -0,0 +1,16 @@
+package factoryPattern;
+
+public class ShapeFactory {
+    public Shape getShape(String type){
+        if(type == null)
+            return null;
+        if(type.equalsIgnoreCase("Circle")){
+            return new Circle();
+        } else if (type.equalsIgnoreCase("Square")){
+            return new Square();
+        } else if (type.equalsIgnoreCase("Rectangle")){
+            return new Rectangle();
+        }
+        return null;
+    }
+}
---
+++ FactoryPattern/src/factoryPattern/Square.java
@@ -0,0 +1,10 @@
+package factoryPattern;
+
+public class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+
+}
